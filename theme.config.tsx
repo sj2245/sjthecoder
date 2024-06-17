a246fd242ff67a4d1f0ff5c1ab3a6455a52acbfd
sj2245 <shakespearejamesii@gmail.com>
2024-06-17 19:42:32 -0400

    Initializing New Website Stuff

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>SJ The Coder</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/sj2245',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/sj2245/sjthecoder',
   footer: {
     text: 'Nextra Docs Template',
   },
