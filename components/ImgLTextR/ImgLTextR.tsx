60c8f3c8667a844a41f87f34d46ee6ec2a763369
sj2245 <shakespearejamesii@gmail.com>
2024-06-22 17:05:34 -0400

    Added ImgLTextR function to ease further coding.Utilized function on About me page.

---
+++ components/ImgLTextR/ImgLTextR.tsx
@@ -0,0 +1,18 @@
+
+interface ImgLTextRProps {
+  imgSrc: string;
+  text1: string;
+  text2: string;
+}
+
+const ImgLTextR: React.FC<ImgLTextRProps> = ({ imgSrc, text1, text2 }) => {
+  return <>
+    <section className={`textbox`}>
+      <img src={imgSrc}></img>
+      <div>
+        <p>{text1}</p>
+        <p>{text2}</p>
+      </div>
+    </section>
+  </>
+}
