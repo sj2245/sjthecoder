384e70713421e017567d0bc8b9457a2db54e187a
sj2245 <shakespearejamesii@gmail.com>
2024-06-30 12:06:55 -0400

    sjthecoder: #20 & #21, Removing Search Bar for now and Fixing Blog Article Link with Dynamic Route

---
+++ server.ts
@@ -0,0 +1,18 @@
+export const blogPosts = [
+  {
+    id: 1,
+    title: `Crushing it with the M6A1 and T1E1 in war thunder`,
+    image: `/assets/images/article1CoverIMG.PNG`,
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, atque vitae? Nulla accusamus fugiat veritatis, dolores, odit delectus vero assumenda, eos omnis rem non autem! Sit, id quidem aliquam deleniti laboriosam non atque sint necessitatibus ratione quibusdam commodi voluptate! Non, possimus alias nemo consectetur aliquid dolores ea amet ullam atque.`
+  },
+  {
+    id: 2,
+    title: `Blog 2`,
+    image: `/assets/images/article1CoverIMG.PNG`,
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, atque vitae? Nulla accusamus fugiat veritatis, dolores, odit delectus vero assumenda, eos omnis rem non autem! Sit, id quidem aliquam deleniti laboriosam non atque sint necessitatibus ratione quibusdam commodi voluptate! Non, possimus alias nemo consectetur aliquid dolores ea amet ullam atque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, atque vitae? Nulla accusamus fugiat veritatis, dolores, odit delectus vero assumenda, eos omnis rem non autem! Sit, id quidem aliquam deleniti laboriosam non atque sint necessitatibus ratione quibusdam commodi voluptate! Non, possimus alias nemo consectetur aliquid dolores ea amet ullam atque`
+  },
+];
+
+// export const getBlogPosts = async () => {
+//   return blogPosts;
+// }
